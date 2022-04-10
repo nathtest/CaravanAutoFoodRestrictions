a6ae36676c78df19d3f177b63248e2aa2165c36a
nath <bobap@hotmail.com>
2022-04-10 17:31:02 +0200

    init

---
+++ Source/CaravanAutoFoodRestrictions/CaravanAutoFoodRestriction.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+using SettingsHelper;
+
+
+namespace CaravanAutoFoodRestrictions
+{
+    [StaticConstructorOnStartup]
+    public static class CaravanAutoFoodRestrictions
+    {
+        static CaravanAutoFoodRestrictions()
+        {
+            Log.Message("CaravanAutoFoodRestrictions StartUp");
+            var harmony = new Harmony("com.caravan.auto.food.restrictions.patch");
+            Log.Message("CaravanAutoFoodRestrictions Harmony instance loaded");
+            harmony.PatchAll();  // todo patch only created methods
+            Log.Message("CaravanAutoFoodRestrictions Harmony PatchAll called");
+        }
+        
+        [HarmonyPatch(typeof(CaravanMaker))]
+        [HarmonyPatch(nameof(CaravanMaker.MakeCaravan))]
+        static class CaravanMaker_MakeCaravan_Patch
+        {
+            static void Postfix(ref Caravan __result) 
+            {
+                Log.Message("CaravanAutoFoodRestrictions Harmony CaravanMaker MakeCaravan");
+                Log.Message("CaravanAutoFoodRestrictions Number of pawns in caravan " + __result.pawns.Count);
+                
+                var pawnFoodRestrictionLabel = LoadedModManager.GetMod<CaravanAutoFoodRestrictionsMod>().GetSettings<CaravanAutoFoodRestrictionsSettings>().PawnFoodRestrictionLabel;
+                if (pawnFoodRestrictionLabel == null)
+                {
+                    Log.Warning("No food restriction selected in setting. Exiting...");
+                    return;
+                }
+                var defaultFoodRestriction = Current.Game.foodRestrictionDatabase.DefaultFoodRestriction();
+                var pawnFoodRestriction = Current.Game.foodRestrictionDatabase.AllFoodRestrictions.FirstOrFallback(restriction => restriction.label == pawnFoodRestrictionLabel, defaultFoodRestriction);
+                
+                Log.Message("CaravanAutoFoodRestrictions loaded setting pawnFoodRestrictionLabel " + pawnFoodRestrictionLabel);
+
+                foreach (var pawn in __result.pawns)
+                {
+                    if (!pawn.RaceProps.Humanlike) continue;
+                    Log.Message("CaravanAutoFoodRestrictions pawn " + pawn.Name + " set food restriction from " + pawn.foodRestriction.CurrentFoodRestriction.label + " to " +  pawnFoodRestriction.label);
+                    pawn.foodRestriction.CurrentFoodRestriction = pawnFoodRestriction;
+                }
+            }
+        }
+    }
+    
+    public class CaravanAutoFoodRestrictionsSettings : ModSettings
+    {
+        public string PawnFoodRestrictionLabel;
+        
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref PawnFoodRestrictionLabel, "PawnFoodRestrictionLabel");
+            Log.Message("CaravanAutoFoodRestrictions ModSettings " + PawnFoodRestrictionLabel);
+            base.ExposeData();
+        }
+    }
+
+    public class CaravanAutoFoodRestrictionsMod : Mod
+    {
+        CaravanAutoFoodRestrictionsSettings settings;
+
+        public CaravanAutoFoodRestrictionsMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<CaravanAutoFoodRestrictionsSettings>();
+        }
+        
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            if (Current.Game != null)
+            {
+                var labelFoodRestrictions = Current.Game.foodRestrictionDatabase.AllFoodRestrictions.Select(foodRestriction => foodRestriction.label).ToArray();
+                listingStandard.AddLabeledRadioList("Pawn Food Restriction", labelFoodRestrictions,ref settings.PawnFoodRestrictionLabel);
+            }
+            else
+            {
+                listingStandard.Label("A save game must be loaded.");
+            }
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+        
+        public override string SettingsCategory()
+        {
+            return "Caravan Auto Food Restrictions".Translate();
+        }
+    }
+}
