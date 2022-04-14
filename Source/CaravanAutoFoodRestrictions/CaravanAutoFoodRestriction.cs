04fbd9ece0a1bbe17fe98f0b1216e8cd264f0177
nath <bobap@hotmail.com>
2022-04-14 23:41:20 +0200

    added new Preview.png and remove caravan_id in dictionary key (useless and caravan splitting bug)

--- Source/CaravanAutoFoodRestrictions/CaravanAutoFoodRestriction.cs
+++ Source/CaravanAutoFoodRestrictions/CaravanAutoFoodRestriction.cs
@@ -17,9 +17,9 @@ namespace CaravanAutoFoodRestrictions
         {
             Log.Message("CaravanAutoFoodRestrictions StartUp");
             var harmony = new Harmony("com.caravan.auto.food.restrictions.patch");
-            Log.Message("CaravanAutoFoodRestrictions Harmony instance loaded");
+            // Log.Message("CaravanAutoFoodRestrictions Harmony instance loaded");
             harmony.PatchAll();  // todo patch only created methods
-            Log.Message("CaravanAutoFoodRestrictions Harmony PatchAll called");
+            // Log.Message("CaravanAutoFoodRestrictions Harmony PatchAll called");
         }
         
         [HarmonyPatch(typeof(CaravanMaker))]
@@ -28,15 +28,15 @@ namespace CaravanAutoFoodRestrictions
         {
             static void Postfix(ref Caravan __result) 
             {
-                Log.Message("CaravanAutoFoodRestrictions Harmony CaravanMaker MakeCaravan");
-                Log.Message("CaravanAutoFoodRestrictions Number of pawns in caravan " + __result.pawns.Count);
+                // Log.Message("CaravanAutoFoodRestrictions Harmony CaravanMaker MakeCaravan");
+                // Log.Message("CaravanAutoFoodRestrictions Number of pawns in caravan " + __result.pawns.Count);
                 
                 var caravanAutoFoodRestrictionsData = Find.World.GetComponent<CaravanAutoFoodRestrictionsData>();
                 
                 if (caravanAutoFoodRestrictionsData.RetainedCaravanData ==  null)
                 {
                     caravanAutoFoodRestrictionsData.RetainedCaravanData = new Dictionary<string, string>();
-                    Log.Message("CaravanAutoFoodRestrictions loaded setting RetainedCaravanData empty");
+                    // Log.Message("CaravanAutoFoodRestrictions loaded setting RetainedCaravanData empty");
                 }
                 
                 var pawnFoodRestrictionLabel = caravanAutoFoodRestrictionsData.PawnFoodRestrictionLabel;
@@ -47,14 +47,14 @@ namespace CaravanAutoFoodRestrictions
                 }
 
                 var pawnFoodRestriction = Current.Game.foodRestrictionDatabase.AllFoodRestrictions.FirstOrFallback(restriction => restriction.label == pawnFoodRestrictionLabel, Current.Game.foodRestrictionDatabase.DefaultFoodRestriction());
-                Log.Message("CaravanAutoFoodRestrictions loaded setting pawnFoodRestrictionLabel " + pawnFoodRestrictionLabel);
+                // Log.Message("CaravanAutoFoodRestrictions loaded setting pawnFoodRestrictionLabel " + pawnFoodRestrictionLabel);
                 
                 foreach (var pawn in __result.pawns)
                 {
                     if (!pawn.RaceProps.Humanlike) continue;
-                    caravanAutoFoodRestrictionsData.RetainedCaravanData[__result.GetUniqueLoadID()+"_"+pawn.GetUniqueLoadID()] = pawn.foodRestriction.CurrentFoodRestriction.label;
+                    caravanAutoFoodRestrictionsData.RetainedCaravanData[pawn.GetUniqueLoadID()] = pawn.foodRestriction.CurrentFoodRestriction.label;
                     
-                    Log.Message("CaravanAutoFoodRestrictions pawn " + pawn.Name + " set food restriction from " + pawn.foodRestriction.CurrentFoodRestriction.label + " to " +  pawnFoodRestriction.label);
+                    // Log.Message("CaravanAutoFoodRestrictions pawn " + pawn.Name + " set food restriction from " + pawn.foodRestriction.CurrentFoodRestriction.label + " to " +  pawnFoodRestriction.label);
                     pawn.foodRestriction.CurrentFoodRestriction = pawnFoodRestriction;
                 }
             }
@@ -66,7 +66,7 @@ namespace CaravanAutoFoodRestrictions
         {
             static void Prefix(Caravan caravan, ref MapParent ___mapParent) 
             {
-                Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter Arrived");
+                // Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter Arrived");
                 
                 Map map = ___mapParent.Map;
                 if (map == null)
@@ -77,26 +77,26 @@ namespace CaravanAutoFoodRestrictions
 
                 if (map.IsPlayerHome)
                 {
-                    Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter caravan home");
+                    // Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter caravan home");
 
                     foreach (var pawn in caravan.pawns)
                     {
-                        if(caravanAutoFoodRestrictionsData.RetainedCaravanData.TryGetValue(caravan.GetUniqueLoadID()+"_"+pawn.GetUniqueLoadID(), out var pawnFoodRestrictionLabelSaved))
+                        if(caravanAutoFoodRestrictionsData.RetainedCaravanData.TryGetValue(pawn.GetUniqueLoadID(), out var pawnFoodRestrictionLabelSaved))
                         {
-                            Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter value found " + pawnFoodRestrictionLabelSaved);
+                            // Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter value found " + pawnFoodRestrictionLabelSaved);
                             pawn.foodRestriction.CurrentFoodRestriction = Current.Game.foodRestrictionDatabase.AllFoodRestrictions.FirstOrFallback(restriction => restriction.label == pawnFoodRestrictionLabelSaved, Current.Game.foodRestrictionDatabase.DefaultFoodRestriction());
-                            caravanAutoFoodRestrictionsData.RetainedCaravanData.Remove(caravan.GetUniqueLoadID()+"_"+pawn.GetUniqueLoadID());
+                            caravanAutoFoodRestrictionsData.RetainedCaravanData.Remove(pawn.GetUniqueLoadID());
                         }
-                        else
-                        {
-                            Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter value not found " + caravan.GetUniqueLoadID()+"_"+pawn.GetUniqueLoadID());
-                        }
+                        // else
+                        // {
+                        //     Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter value not found " + pawn.GetUniqueLoadID());
+                        // }
                     }
                 }
-                else
-                {
-                    Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter caravan is not home");
-                }
+                // else
+                // {
+                //     Log.Message("CaravanAutoFoodRestrictions Harmony CaravanArrivalAction_Enter caravan is not home");
+                // }
             }
         }
         
@@ -132,7 +132,7 @@ namespace CaravanAutoFoodRestrictions
             if (Current.Game != null)
             {
                 var labelFoodRestrictions = Current.Game.foodRestrictionDatabase.AllFoodRestrictions.Select(foodRestriction => foodRestriction.label).ToArray();
-                listingStandard.AddLabeledRadioList("Pawn Food Restriction", labelFoodRestrictions,ref Find.World.GetComponent<CaravanAutoFoodRestrictionsData>().PawnFoodRestrictionLabel);
+                listingStandard.AddLabeledRadioList("Default Caravan Food Restriction", labelFoodRestrictions,ref Find.World.GetComponent<CaravanAutoFoodRestrictionsData>().PawnFoodRestrictionLabel);
             }
             else
             {
